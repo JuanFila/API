d2ef8c280383bca6a5d5f86e5e972ceee75d4614
JuanFila <juanfilapablo@hotmail.com>
2023-11-29 21:42:38 -0300

    exemple middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,18 @@
 const {Router} = require('express')
+const UsersController = require("../controllers/UsersController")
+
 const usersRoute = Router()
 
-const UsersController = require("../controllers/UsersController")
+function myMiddleware(request, response, next) {
+  console.log("Passou pelo middleware!");
+  if(!request.body.iSAdmin){
+    return response.json({message: "user unauthorized"})
+  }
+  next()
+}
 
 const userControler = new UsersController();
 
-usersRoute.post("/", userControler.create)
+usersRoute.post("/", myMiddleware, userControler.create)
 
 module.exports = usersRoute
