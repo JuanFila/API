65f4e5f35d18465a380b89ad4f4d85f347ac6951
JuanFila <juanfilapablo@hotmail.com>
2023-12-12 22:50:52 -0300

    add bcrypt

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,12 +1,23 @@
+const {hash} = require("bcryptjs")
 const AppError = require("../utils/AppError") 
+
+const sqliteConnection = require("../database/sqlite")
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
-        if(!name){
-            throw new AppError("O nome é obrigadatório!");
+        const database = await sqliteConnection();
+        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(checkUserExist){
+            throw new AppError("Este e-mail já está em uso")
         }
-        response.status(201).json({ name, email, password })
+
+        const hashedPassword = await hash(password, 8)
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (? , ?, ?)", [name, email, hashedPassword])
+
+        return response.status(201).json()
     }
 }
 
