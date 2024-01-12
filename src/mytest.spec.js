0fd3866573dd6554fab2161830b525017cad5add
JuanFila <juanfilapablo@hotmail.com>
2024-01-11 22:46:10 -0300

    add test exemple

---
+++ src/mytest.spec.js
@@ -0,0 +1,8 @@
+it("result of them sum of 2 + 2 must be 4", () => {
+    const a = 2;
+    const b = 2;
+    const result = a + b;
+
+    expect(result).toEqual(4) // expect = minha expectativa / to equal resultado
+})
+// it recebe 2 paramretros um deles é a descrição e depois a função
