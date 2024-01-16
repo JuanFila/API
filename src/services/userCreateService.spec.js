624cfd2000c01c989561e7babb6068d2b24d1792
JuanFila <juanfilapablo@hotmail.com>
2024-01-15 22:20:49 -0300

    update

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -3,17 +3,23 @@ const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
 const AppError = require('../utils/AppError');
 
 describe("UserCreateService", () => { // describe cria grupos de teste especifico para um mesmo assunto
+    
+    let userRepositoryInMemory = null;
+    let userCreateService = null;
+
+    beforeEach(() => { // beforeEach é uma função do jest que executa antes de cada teste ser rodados 
+         userRepositoryInMemory = new UserRepositoryInMemory(); // estamos utilizando essas 2 instancias aqui dentro para não precisar repetir essas duas linhas dentro de cada teste
+         userCreateService = new UserCreateService(userRepositoryInMemory);
+    });
+
     it("user should be create", async () => {
        const user = {
            name: "user teste",
            email: "user.teste@teste.com",
            password: "123"
        };
-       const userRepositoryInMemory = new UserRepositoryInMemory(); // meu banco em memoria para teste
-       const userCreateService = new UserCreateService(userRepositoryInMemory);// usamos a regra de negocio
+       
        const userCreated = await userCreateService.execute(user)
-    
-       //console.log(userCreated)
        expect(userCreated).toHaveProperty("id") // expect = minha expectativa / to equal / 
     })
     // it recebe 2 paramretros um deles é a descrição e depois a função
@@ -30,11 +36,8 @@ describe("UserCreateService", () => { // describe cria grupos de teste especific
             password: "456"
         };
 
-        const userRepository = new UserRepositoryInMemory();
-        const userCreateService = new UserCreateService(userRepository);
-
         await userCreateService.execute(user1);
-        await expect(userCreateService.execute(user1)).rejects.toEqual(new AppError("Este e-mail já está em uso"))
+        await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso"))
         
     })
 })
